5a490749600448e6237c06eb0bd1fc551aa803a9
fengmk2 <fengmk2@gmail.com>
2014-03-10 17:25:50 +0800

    first commit

---
+++ test/readall.test.js
@@ -0,0 +1,84 @@
+/**!
+ * readall - test/readall.test.js
+ *
+ * Copyright(c) 2014 fengmk2 and other contributors.
+ * MIT Licensed
+ *
+ * Authors:
+ *   fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
+ */
+
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+
+var fs = require('fs');
+var http = require('http');
+var EventEmitter = require('events').EventEmitter;
+var should = require('should');
+var readall = require('../');
+
+describe('readall.test.js', function () {
+  it('should read file stream success', function (done) {
+    readall(fs.createReadStream(__filename), function (err, data) {
+      should.not.exist(err);
+      data.should.be.a.Buffer;
+      data.should.length(fs.statSync(__filename).size);
+      done();
+    });
+  });
+
+  it('should read not file stream error', function (done) {
+    readall(fs.createReadStream(__filename + 'notexist'), function (err, data) {
+      should.exist(err);
+      err.message.should.include('ENOENT');
+      should.not.exist(data);
+      done();
+    });
+  });
+
+  it('should read http response stream success', function (done) {
+    http.get('http://r.cnpmjs.org/byte', function (res) {
+      readall(res, function (err, data) {
+        should.not.exist(err);
+        data.should.be.a.Buffer;
+        done();
+      });
+    });
+  });
+
+  it('should mock stream error', function (done) {
+    var stream = new EventEmitter();
+    stream.read = function () {
+      return null;
+    };
+
+    readall(stream, function (err, data) {
+      should.exist(err);
+      err.message.should.equal('mock error');
+      should.not.exist(data);
+      done();
+    });
+
+    stream.emit('error', new Error('mock error'));
+    stream.emit('end');
+  });
+
+  it('should mock stream empty', function (done) {
+    var stream = new EventEmitter();
+    stream.read = function () {
+      stream.read = function () {};
+      return new Buffer('');
+    };
+
+    readall(stream, function (err, data) {
+      should.not.exist(err);
+      should.not.exist(data);
+      done();
+    });
+
+    stream.emit('end');
+  });
+});
